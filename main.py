063a51f9b08f187d721becb51b56facb8001e92a
geekyabs <abeshchakraborty10@gmail.com>
2026-09-18 23:05:00 +0530

    donwloading weights from hf

---
+++ main.py
@@ -0,0 +1,12 @@
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
+
+model = GPT2LMHeadModel.from_pretrained("gpt2")
+
+for name, param in model.named_parameters():
+    print(name, tuple(param.shape))
+    values = param.detach().numpy().flatten()
+    with open(f"../weights/{name}.txt", "w") as f:
+        f.write(" ".join(str(v) for v in values))
+
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+tokenizer.save_pretrained("../weights/tokenizer")
